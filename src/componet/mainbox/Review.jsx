607674d770aeeeaa083d8ed6e407829891bee750
mdsaiful170 <saifulsylhet00@gmail.com>
2024-11-25 00:48:04 +0600

    all done

--- src/componet/mainbox/Review.jsx
+++ src/componet/mainbox/Review.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+import { Reviewdata } from "../../../lib/db/datadb";
+import { Reviewbox } from "../../share/Reviewbox";
 import { Container } from "../compobox/Container";
 import { Heading } from "../compobox/Heading";
+import { Star } from "../compobox/star";
+import { cn } from "../../../lib/utilites/cn";
 
 const Review = () => {
+  const [active, setactive] = useState(1);
+  const review = Reviewdata[active];
+
   return (
     <>
-      <section className="lg:pt-30 md:pt-20 pt-14">
+      <section className="my-[150px] py-20 bg-[#F6F6F6]">
         <Container>
-          <div>
-            <Heading gradinet={"Our Client"}>Review From</Heading>
-            <div className=" flex items-center justify-center gap-[50px] md:gap-[60px] lg:gap-[93px]">
-              <div>
+          <Heading gradinet={"Out Client"} className={"text-center"}>
+            Reviews Froms{" "}
+          </Heading>
+          <div className="pt-10 flex items-center justify-center gap-10 lg:gap-[80px] xl:gap-[93px]">
+            <div className="flex w-full gap-5">
+              <div className="bg-[#E2DED8] w-[5px] rounded-[5px]">
+                <div className="space-y-[15px]">
+                  {Array.from(Array(3).keys()).map((el) => (
+                    <div
+                      key={el}
+                      className={cn(
+                        "w-[5px] h-[92px] rounded-[20px]  bg-transparent",
+                        active === el && "bg-textblue"
+                      )}
+                    ></div>
+                  ))}
+                </div>
+              </div>
 
+              <div className="space-y-[13px] flex-grow">
+                {Reviewdata.map((res, i) => (
+                  <div key={i}>
+                    <Reviewbox
+                      {...res}
+                      active={active === i}
+                      onClick={() => setactive(i)}
+                    />
+                  </div>
+                ))}
               </div>
-              <div>
-                
+            </div>
+
+            <div className="w-full space-y-[20px]">
+              <h2 className="text-[30px] text-textsecondary font-bold">
+                {review.review.title}
+              </h2>
+              <div className="flex gap-[5px]">
+                {Array.from(Array(review.review.stars).keys()).map((el) => (
+                  <Star key={el} className={"text-[#FC950F] size-5 "} />
+                ))}
               </div>
+              <p className="text-base font-semibold text-textprimary max-w-[555px]">
+                {review.review.details}
+              </p>
             </div>
           </div>
         </Container>
--- src/share/Reviewbox.jsx
+++ src/share/Reviewbox.jsx
@@ -1,11 +1,20 @@
+import { cn } from "../../lib/utilites/cn";
 
-
-export const Reviewbox = () => {
+export const Reviewbox = (props) => {
+  const { onClick, img, name, hp,  active } = props || {};
   return (
     <>
-      
+      <button onClick={onClick} className={cn("inline-flex items-center px-5 py-[12px] justify-start bg-white gap-[13px] border border-textblue rounded-[20px] w-full md:w-[400px] lg:w-[491px]" , active && 'gradient-one')}>
+        <img
+          src={img}
+          alt=""
+          className="rounded-full object-cover size-[68px]"
+        />
+        <div className={cn("text-textprimary text-base font-semibold text-left", active && 'text-white')}>
+          <h4>{name}</h4>
+          <p>{hp}</p>
+        </div>
+      </button>
     </>
-  )
-}
-
-
+  );
+};
